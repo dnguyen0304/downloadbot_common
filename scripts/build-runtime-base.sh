5294d684fa81069d6258bd0e07ca5dda656593a1
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-10 15:16:17 -0500

    ME-729: Changed naming convention from COMPONENT to PACKAGE_NAME.

--- scripts/build-runtime-base.sh
+++ scripts/build-runtime-base.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-COMPONENT=$(./scripts/get-package-name.sh)
+PACKAGE_NAME=$(./scripts/get-package-name.sh)
 TAG=$(./scripts/get-package-version.sh)
 
 docker build \
     --file docker/runtime/base/Dockerfile \
-    --tag dnguyen0304/${COMPONENT}-runtime-base:${TAG} \
-    --build-arg COMPONENT=${COMPONENT} \
+    --tag dnguyen0304/${PACKAGE_NAME}-runtime-base:${TAG} \
+    --build-arg PACKAGE_NAME=${PACKAGE_NAME} \
     .
